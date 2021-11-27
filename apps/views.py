0bc5a942dbc3f591fa10e9fd73bc249d769c2672
Ahmad Zulfadli <ponyongekruso34@gmail.com>
2021-11-27 11:54:13 +0700

    login registrasi

--- apps/views.py
+++ apps/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render
+from apps.forms import *
 
+#=================================registrasi==========================================
+def login(request):
+    contex={}
+
+    return render(request, 'user/login.html', contex) 
+
+def registrasi(request):
+    contex={}
+
+    return render(request, 'user/registrasi.html', contex) 
+
+def logout(request):
+    contex={}
+
+    return render(request, 'user/logout.html', contex) 
+#=====================================================================================
+
+#====================================template=========================================
 def home(request):
     contex = {}
 
     return render(request, 'index.html', contex)
 
 def pengajuan(request):
-    contex={}
+    proposal = dataUserForm()
+    contex={
+        'proposal':proposal
+    }
 
     return render(request, 'input.html', contex)
 
@@ -14,3 +36,10 @@ def panduan(request):
     contex={}
 
     return render(request, 'panduan.html', contex)
+#=====================================================================================
+
+#=====================================================================================
+#=====================================================================================
+
+#=====================================================================================
+#=====================================================================================
--- input_proposal/urls.py
+++ input_proposal/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('home/', home, name="home"),
     path('input/', pengajuan, name="input"),
     path('panduan/', panduan, name="panduan"),
+
+    path('login/', login, name="login"),
+    path('logout/', logout, name="logout"),
+    path('registrasi/', registrasi, name="registrasi"),
 ]
